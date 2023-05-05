4ac1eec4bd78853eeaa549c1b022e7313028c982
nazianze <victor.nazianzeno-lejamtel@epfl.ch>
2023-05-05 14:27:37 +0200

    format change

--- website/js/comparison.js
+++ website/js/comparison.js
@@ -1,26 +1,30 @@
 const csvArabica = "https://raw.githubusercontent.com/com-480-data-visualization/project-2023-vireal/master/data/coffee_quality/arabica_data_cleaned.csv";
 const csvRobusta = "https://raw.githubusercontent.com/com-480-data-visualization/project-2023-vireal/master/data/coffee_quality/robusta_data_cleaned.csv";
 
-d3.csv(csvArabica).then(function(data) {loadQualityDB(data,"arabica")});
-d3.csv(csvRobusta).then(function(data) {loadQualityDB(data,"robusta")});
+d3.csv(csvArabica).then(function (data) {
+    loadQualityDB(data, "arabica")
+});
+d3.csv(csvRobusta).then(function (data) {
+    loadQualityDB(data, "robusta")
+});
 
-loadQualityDB = function(data,name) {
-  // Extract the columns we're interested in
-  const columns = ["Aroma","Flavor","Aftertaste","Acidity","Balance","Sweetness"];
-  const columnData = data.map(function(d) {
-    return columns.map(function(column) {
-      return parseFloat(d[column]);
+loadQualityDB = function (data, name) {
+    // Extract the columns we're interested in
+    const columns = ["Aroma", "Flavor", "Aftertaste", "Acidity", "Balance", "Sweetness"];
+    const columnData = data.map(function (d) {
+        return columns.map(function (column) {
+            return parseFloat(d[column]);
+        });
     });
-  });
 
-  // Compute the means of each column
-  const means = columns.map(function(column, i) {
-    const values = columnData.map(function(row) {
-      return row[i];
+    // Compute the means of each column
+    const means = columns.map(function (column, i) {
+        const values = columnData.map(function (row) {
+            return row[i];
+        });
+        const sum = d3.sum(values);
+        return {name: column, mean: (sum / values.length).toFixed(2)};
     });
-    const sum = d3.sum(values);
-    return { name: column, mean: (sum / values.length).toFixed(2) };
-  });
 
-  console.log(means);
+    console.log(means);
 };
